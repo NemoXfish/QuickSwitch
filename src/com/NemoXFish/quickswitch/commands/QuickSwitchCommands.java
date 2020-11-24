b9bb4c82efc6744611841a56bb6555b9f6284b0b
NemoXfish <nemodang2@gmail.com>
2020-11-24 15:29:53 -0800

    added chat messages when cmds are sent

--- src/com/NemoXFish/quickswitch/commands/QuickSwitchCommands.java
+++ src/com/NemoXFish/quickswitch/commands/QuickSwitchCommands.java
@@ -10,21 +10,26 @@ public class QuickSwitchCommands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use that command!");
             return true;
         }
         Player player = (Player) sender;
 
         if (cmd.getName().equalsIgnoreCase("gmc")) {
             player.setGameMode(GameMode.CREATIVE);
+            player.sendMessage("§fSet own game mode to Creative Mode");
         }
         else if (cmd.getName().equalsIgnoreCase("gms")) {
             player.setGameMode(GameMode.SURVIVAL);
+            player.sendMessage("§fSet own game mode to Survival Mode");
         }
         else if (cmd.getName().equalsIgnoreCase("gma")) {
             player.setGameMode(GameMode.ADVENTURE);
+            player.sendMessage("§fSet own game mode to Adventure Mode");
         }
         else if (cmd.getName().equalsIgnoreCase("gmsp")) {
             player.setGameMode(GameMode.SPECTATOR);
+            player.sendMessage("§fSet own game mode to Spectator Mode");
         }
 
         return true;
